d4ac5987583fcd9c48364337b38a068cca71a2d0
Kevin Sun <kevinsun0@gmail.com>
2017-11-17 12:16:09 -0500

    oauth

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -9,7 +9,7 @@ import json
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db import models
-from oauth2client.contrib.django_util.models import CredentialsField
+# from oauth2client.contrib.django_util.models import CredentialsField
 from math import sqrt
 import logging
 from .Extension_Models import *
